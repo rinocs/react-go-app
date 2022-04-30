2c9a9d4d702228557f84ff3f97cdef83f6b506ab
Riccardo Nocella <riccardo.nocella@sensoworks.com>
2022-04-30 16:47:59 +0200

    chat app

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => (
+    <div className="header">
+        <h2>Real time chat app</h2>
+    </div>
+);
+
+
+
+export default Header;
+
+
+
